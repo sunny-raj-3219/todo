1a7c9bdb796732c5eb64718b2aa27398579f6bf3
Your Name <your.email@example.com>
2025-05-28 23:59:53 +0530

    Removed invalid axios call from backend

--- Server/index.js
+++ Server/index.js
@@ -1,40 +1,41 @@
-const express = require('express')
-const mongoose = require ('mongoose')
-const cors = require('cors')
-const TodoModel = require('./Models/Todo')
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const TodoModel = require('./Models/Todo');
 
-const app = express()
-app.use(cors())
-app.use(express.json())
+const app = express();
+app.use(cors());
+app.use(express.json());
 
-//mongoose.connect('mongodb://127.0.0.1:27017/test')
-axios.post('https://todo-backnd.onrender.com/todos', newTodo);
-
-
-app.get('/get', (req, res)=>{
-    TodoModel.find()
-    .then(result=>res.json(result))
-    .catch(err=>res.status(500).json(err))
+// Connect to MongoDB
+mongoose.connect('mongodb+srv://rajsunny0022:Sunny111@@cluster.mongodb.net/test?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 })
+.then(() => console.log("Connected to MongoDB"))
+.catch((err) => console.error("MongoDB connection error:", err));
 
+app.get('/get', (req, res) => {
+  TodoModel.find()
+    .then(result => res.json(result))
+    .catch(err => res.status(500).json(err));
+});
 
 app.put('/update/:id', async (req, res) => {
   try {
     const todo = await TodoModel.findById(req.params.id);
     if (!todo) return res.status(404).json({ error: "Todo not found" });
 
-    todo.done = !todo.done; // Toggle done
+    todo.done = !todo.done;
     await todo.save();
-
     res.json(todo);
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
 });
 
-
 app.post('/add', (req, res) => {
-  console.log("Request received:", req.body); // Debug log
+  console.log("Request received:", req.body);
   TodoModel.create({ task: req.body.task })
     .then(result => res.json(result))
     .catch(err => res.status(500).json(err));
@@ -47,7 +48,6 @@ app.delete('/delete/:id', (req, res) => {
     .catch(err => res.status(500).json(err));
 });
 
-
 app.listen(3001, () => {
-    console.log("Server is Running")
-})
+  console.log("Server is Running on port 3001");
+});
